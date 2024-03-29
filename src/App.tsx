0c0c1781c38bbf82bccfb2926ce27e464b0077e9
lyh <1114858097@qq.com>
2024-03-29 17:15:32 +0800

    fix: 测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import moment from "moment";
 
 import "./App.css";
 
 const App: React.FC = () => {
-  const day = moment().diff("2022-06-22", "days");
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          这是我们在一起的第
-          <span style={{ color: "red", fontSize: "60px" }}>{day}</span>天
-        </p>
+        <p>测试</p>
       </header>
     </div>
   );
